703847877a8b70d82d40f75e65e261fa15740b54
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-07-13 22:54:57 +0700

    add clients

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/dashboard/clients', function(Request $request){
+    return view('clients', [
+        'clients' => $request->user()->clients
+    ]);
+})->middleware(['auth'])->name('dashboard.clients');
 
 require __DIR__.'/auth.php';
