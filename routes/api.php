f974b2f15c1039adacae67a047d99a3dd0ba910b
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-07-14 22:19:14 +0700

    add post

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/posts', function(){
+    return Post::with('user')->get()->toJson();
+});
